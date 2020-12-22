064a42b2a42fcf1807fe8cc57433a060bece11dc
Yaroslav Fedoriachenko <yar.fed99@gmail.com>
2020-12-23 00:54:42 +0200

    task02: implemet number guessing
    Signed-off-by: Yaroslav Fedoriachenko <yar.fed99@gmail.com>

--- task02/task02.sh
+++ task02/task02.sh
@@ -5,5 +5,16 @@ function genrand ()
 	echo $(seq 0 5 | shuf - | head -1)
 }
 
-echo $(genrand)
+function compare_numbers ()
+{
+	if [[ $1 -lt $2 ]]; then
+		echo "$1 is less than $2"
+	elif [[ $1 -gt $2 ]]; then
+		echo "$1 is greater than $2"
+	else
+		echo "$1 is equal to $2"
+	fi
+}
+
+compare_numbers "$1" "$(genrand)"
 
