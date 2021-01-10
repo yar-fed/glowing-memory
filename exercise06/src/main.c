647953697da2656972903c5d75d1a07b0fdca618
Yaroslav Fedoriachenko <yar.fed99@gmail.com>
2021-01-10 04:54:15 +0200

    exercise06: fix contact bounce of user_button with timer interrupt
    Timer TIM10's counter is set to 0 on key_press in main routine and
    in 50 ms happens its update interrupt. The assosiated isr checks if
    key is still pressed and sets user_key_down flag.
    If key is not pressed, main routine checks for user_key_down flag and
    if it is set, changes led state.
    
    Signed-off-by: Yaroslav Fedoriachenko <yar.fed99@gmail.com>

--- exercise06/src/main.c
+++ exercise06/src/main.c
@@ -3,6 +3,8 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/f4/nvic.h>
 #include "../libopencm3-drivers/include/keys.h"
 
 
@@ -13,10 +15,12 @@ static struct keys_s key = {.port = GPIOA,
 
 static uint32_t led_pins[] = {GPIO12, GPIO13, GPIO14, GPIO15};
 
-// Variable that used for counting ms
+static volatile uint32_t reading_user_key = false;
+static volatile uint32_t user_key_down = false;
+
 static volatile uint32_t tick_ms_count;
 
-void gpio_setup()
+static void gpio_setup()
 {
 	rcc_periph_clock_enable(RCC_GPIOD);
 	rcc_periph_clock_enable(RCC_GPIOA);
@@ -40,33 +44,80 @@ static void systick_setup(void)
 	systick_counter_enable();
 }
 
+static void nvic_setup(void)
+{
+	nvic_enable_irq(NVIC_TIM1_UP_TIM10_IRQ);
+	nvic_set_priority(NVIC_TIM1_UP_TIM10_IRQ, 1);
+}
+
+static void timer_setup(void)
+{
+	rcc_set_ppre2(RCC_CFGR_PPRE_DIV_16);
+	rcc_periph_clock_enable(RCC_TIM10);
+	timer_set_mode(TIM10, TIM_CR1_CKD_CK_INT,
+		TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+	timer_set_counter(TIM10, 1);
+	timer_set_prescaler(TIM10, 21000);
+	timer_disable_preload(TIM10);
+	timer_set_period(TIM10, 100);
+	timer_enable_irq(TIM10, TIM_DIER_UIE);
+	timer_enable_counter(TIM10);
+}
+
 void sys_tick_handler(void)
 {
 	tick_ms_count++;
 }
 
+void tim1_up_tim10_isr(void)
+{
+	if (timer_get_flag(TIM10, TIM_SR_UIF)) {
+		timer_clear_flag(TIM10, TIM_SR_UIF);
+		if (reading_user_key) {
+			if (key_pressed(&key, 0)) {
+				user_key_down = true;
+			}
+			reading_user_key = false;
+		}
+	}
+}
+
+static void change_led_state(void)
+{
+	static int i = 0;
+
+	i = (i + 1) % 5;
+	if (i != 0) {
+		gpio_set(GPIOD, led_pins[i - 1]);
+	} else {
+		int j;
+		for (j = 0; j < 4; ++j)
+			gpio_clear(GPIOD, led_pins[j]);
+	}
+}
 
 int main(void)
 {
 	rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-	gpio_setup();
 	systick_setup();
+	gpio_setup();
 	keys_setup(&key, 1);
-
-	int i = 0;
-	int j;
+	nvic_setup();
+	timer_setup();
 
 	while (1) {
 		if (key_pressed(&key, 0)) {
-			i = (i + 1) % 5;
-			if (i != 0)
-				gpio_set(GPIOD, led_pins[i - 1]);
-			else
-				for (j = 0; j < sizeof(led_pins); ++j)
-					gpio_clear(GPIOD, led_pins[j]);
+			if (!reading_user_key && !user_key_down) {
+				reading_user_key = true;
+				timer_set_counter(TIM10, 0);
+			}
+		} else {
+			if (user_key_down) {
+				user_key_down = false;
+				change_led_state();
+			}
 		}
 	}
 
 	return 0;
 }
-
