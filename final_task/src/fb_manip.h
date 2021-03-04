5aec4d848419342baceca6722c64dfbd38354ec3
Yaroslav Fedoriachenko <yar.fed99@gmail.com>
2021-03-05 00:15:33 +0200

    fb_manip: add keyboard layouts and a few new functions
    Layouts are structs to access bitmaps for display and symbols to save
    buffer state in the future.
    
    Signed-off-by: Yaroslav Fedoriachenko <yar.fed99@gmail.com>

--- final_task/src/fb_manip.h
+++ final_task/src/fb_manip.h
@@ -6,37 +6,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "layouts.h"
 
-#define WIDTH 160
-
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 128
+#define SCREEN_RESOLUTION SCREEN_WIDTH * SCREEN_HEIGHT
+#define SCREEN_BPP 16
+#define SCREEN_BYTESPP SCREEN_BPP >> 3
+#define SCREEN_BUFFER_SIZE SCREEN_RESOLUTION * SCREEN_BYTESPP
 
 #define RED 0xf800
 #define GREEN 0x07e0
 #define BLUE 0x001f
+#define WHITE 0xffff
+#define BLACK 0x0000
+#define TRANSPARENT 0x0000
+
+#define SWAP(t, x, y) {t _tmp = x; x = y; y = _tmp;}
 
 static int fbfd;
 static __u16 *fbp;
 static struct fb_var_screeninfo vsinfo;
-static struct fb_var_screeninfo vsinfo_bkp;
-
-#define HIR_A_bits12 HIRAGANA_LETTER_A_bits12
-#define HIR_A_bits14 HIRAGANA_LETTER_A_bits14
-#define HIR_W 10
-#define HIR_H 10
-static char HIRAGANA_LETTER_A_bits[] = {
-  0x08, 0x00, 0x08, 0x00, 0xFF, 0x01, 0x08, 0x00, 0x7C, 0x00, 0xA6, 0x00, 
-  0x15, 0x01, 0x15, 0x01, 0x8E, 0x00, 0x60, 0x00, };
-static char HIRAGANA_LETTER_A_bits11[] = {
-  0x00, 0x00, 0x10, 0x00, 0x10, 0x00, 0xFE, 0x03, 0x10, 0x00, 0xF8, 0x00, 
-  0x4C, 0x01, 0x2A, 0x02, 0x2A, 0x02, 0x1C, 0x01, 0xC0, 0x00, 0x00, 0x00};
-static char HIRAGANA_LETTER_A_bits14[] = {
-  0xFF, 0x3F, 0x01, 0x20, 0x21, 0x20, 0x21, 0x20, 0xFD, 0x27, 0x21, 0x20, 
-  0xF1, 0x21, 0x99, 0x22, 0x55, 0x24, 0x55, 0x24, 0x39, 0x22, 0x81, 0x21, 
-  0x01, 0x20, 0xFF, 0x3F, };
 
 static inline void putpixel(int x, int y, __u16 c)
 {
-	fbp[x + y * WIDTH] = c;
+	fbp[x + y * SCREEN_WIDTH] = c;
+}
+
+static inline void drawrect(int x, int y, int w, int h, __u16 c)
+{
+	for (int i = 0; i < w; ++i) {
+		putpixel(x + i, y, c);
+	}
+	for (int i = 1; i < h - 1; ++i) {
+		putpixel(x, y + i, c);
+		putpixel(x + w - 1, y + i, c);
+	}
+	for (int i = 0; i < w; ++i) {
+		putpixel(x + i, y + h - 1, c);
+	}
 }
 
 static inline void fillrect(int x, int y, int w, int h, __u16 c)
@@ -73,9 +81,9 @@ static inline void put_2B_xbm(int point_size, int x, int y, __u16 c, char *bits)
 	}
 }
 
-static inline clear_screen(__u16 c)
+static inline void clear_screen(__u16 c)
 {
-	memset(fbp, c, 1);
+	memset(fbp, c, SCREEN_BUFFER_SIZE);
 }
 
 #define put_10x10xbm_bg(x, y, fg, bg, bits) put_2B_xbm_bg(10, x, y, fg, bg, bits)
@@ -85,13 +93,48 @@ static inline clear_screen(__u16 c)
 #define put_14x14xbm_bg(x, y, fg, bg, bits) put_2B_xbm_bg(14, x, y, fg, bg, bits)
 #define put_14x14xbm(x, y, c, bits) put_2B_xbm(14, x, y, c, bits)
 
+static inline void draw_keyboard(int x, int y, __u16 fg, __u16 bg, __u16 layout)
+{
+	drawrect(x, y, 4*14+2, 4*14+2, fg);
+	put_14x14xbm_bg(x +  1, y +  1, fg, bg, kb_layouts[layout].keys[0]);
+	put_14x14xbm_bg(x + 15, y +  1, fg, bg, kb_layouts[layout].keys[1]);
+	put_14x14xbm_bg(x + 29, y +  1, fg, bg, kb_layouts[layout].keys[2]);
+	put_14x14xbm_bg(x + 43, y +  1, fg, bg, kb_layouts[layout].keys[3]);
+	put_14x14xbm_bg(x +  1, y + 15, fg, bg, kb_layouts[layout].keys[4]);
+	put_14x14xbm_bg(x + 15, y + 15, fg, bg, kb_layouts[layout].keys[5]);
+	put_14x14xbm_bg(x + 29, y + 15, fg, bg, kb_layouts[layout].keys[6]);
+	put_14x14xbm_bg(x + 43, y + 15, fg, bg, kb_layouts[layout].keys[7]);
+	put_14x14xbm_bg(x +  1, y + 29, fg, bg, kb_layouts[layout].keys[8]);
+	put_14x14xbm_bg(x + 15, y + 29, fg, bg, kb_layouts[layout].keys[9]);
+	put_14x14xbm_bg(x + 29, y + 29, fg, bg, kb_layouts[layout].keys[10]);
+	put_14x14xbm_bg(x + 43, y + 29, fg, bg, kb_layouts[layout].keys[11]);
+	put_14x14xbm_bg(x +  1, y + 43, fg, bg, kb_layouts[layout].keys[12]);
+	put_14x14xbm_bg(x + 15, y + 43, fg, bg, kb_layouts[layout].keys[13]);
+	put_14x14xbm_bg(x + 29, y + 43, fg, bg, kb_layouts[layout].keys[14]);
+	put_14x14xbm_bg(x + 43, y + 43, fg, bg, kb_layouts[layout].keys[15]);
+}
+
+static inline void draw_key(int x, int y, __u16 fg, __u16 bg, __u16 key, __u16 layout, int down)
+{
+	if (down)
+		SWAP(__u16, fg, bg);
+	put_14x14xbm_bg(x + 1 + (key & 3) * 14, y + 1 + (key >> 2) * 14, fg, bg, kb_layouts[layout].keys[key]);
+}
+
+static inline void draw_symbol(int x, int y, __u16 fg, __u16 bg, __u16 key, __u16 layout, int selected)
+{
+	if (selected)
+		SWAP(__u16, fg, bg);
+	put_12x12xbm_bg(x + 1 + (key & 3) * 12, y + 1 + (key >> 2) * 12, fg, bg, kb_layouts[layout].keys[key]);
+}
+
 int main(int argc, char *argv[])
 {
 	fbfd = open("/dev/fb1", O_RDWR);
 	if (fbfd == -1)
 		return -1;
 	fbp = (__u16*)mmap(0,
-		128*160,
+		SCREEN_BUFFER_SIZE,
 		PROT_READ | PROT_WRITE,
 		MAP_SHARED,
 		fbfd,
@@ -100,32 +143,16 @@ int main(int argc, char *argv[])
 		printf("Failed to mmap\n");
 		return -2;
 	}
-	//if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vsinfo)) {
-	//	printf("Error reading variable information.\n");
-	//}
-	//memcpy(&vsinfo_bkp, &vsinfo, sizeof(struct fb_var_screeninfo));
-	//vsinfo.rotate = 270;
-	//if (ioctl(fbfd, FBIOPUT_VSCREENINFO, &vsinfo)) {
-	//	printf("Error setting variable information.\n");
-	//}
-	//ioctl(fbfd, FBIO_WAITFORVSYNC, &dummy);
 	__u32 dummy = 0;
-	fillrect(20, 20, 48, 16, 0x0000);
-	put_14x14xbm_bg(20, 20, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(34, 20, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(48, 20, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(20, 34, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(34, 34, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(48, 34, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(20, 48, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(34, 48, 0x07e0, 0x0000, HIR_A_bits14);
-	put_14x14xbm_bg(48, 48, 0x0000, 0x07e0, HIR_A_bits14);
-	ioctl(fbfd, FBIO_WAITFORVSYNC, &dummy);
-	getchar();
-	//if (ioctl(fbfd, FBIOPUT_VSCREENINFO, &vsinfo_bkp)) {
-	//	printf("Error setting variable information.\n");
-	//}
-	munmap(fbp, 128*160);
+	clear_screen(BLACK);
+	draw_keyboard(20, 20, GREEN, BLACK, 0);
+	for (int i = 0; i < 16; ++i) {
+		draw_key(20, 20, GREEN, BLACK, i, 0, 1);
+		ioctl(fbfd, FBIO_WAITFORVSYNC, &dummy);
+		sleep(1);
+		draw_key(20, 20, GREEN, BLACK, i, 0, 0);
+	}
+	munmap(fbp, SCREEN_BUFFER_SIZE);
 	close(fbfd);
 	return 0;
 }
