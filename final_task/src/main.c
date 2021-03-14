52bb5c58a9b153ada8e33f1c62f689adced9e3b3
Yaroslav Fedoriachenko <yar.fed99@gmail.com>
2021-03-14 02:29:26 +0200

    final_task: add functional keys handling, fix multiple bugs
     - Refactor text_buffer and all code related to it in order to have
     an endless buffer using dynamically allocated memory
     - Add handling of all functional keys
     - Change layouts to include katakana_hiragana switch button in all of
     the layouts
     - Add hello message on startup
     - Remove sigint handler
    Space and tilda were not in the font for some reason, so will soon be
    manually drawn. Everything else seems to work as intended.
    
    Signed-off-by: Yaroslav Fedoriachenko <yar.fed99@gmail.com>

--- final_task/src/main.c
+++ final_task/src/main.c
@@ -3,32 +3,37 @@
 #include <signal.h>
 
 // SCREEN_WIDTH / 20
-#define TEXT_BUFFER_WIDTH 8 
-#define TEXT_BUFFER_HEIGHT SCREEN_HEIGHT / 10
-#define TEXT_BUFFER_LENGTH TEXT_BUFFER_HEIGHT * TEXT_BUFFER_WIDTH
+#define TEXT_BUFFER_VISIBLE_WIDTH 8
+//#define TEXT_BUFFER_VISIBLE_HEIGHT SCREEN_HEIGHT / 10
+#define TEXT_BUFFER_VISIBLE_HEIGHT 5
+#define TEXT_BUFFER_VISIBLE_LENGTH                                             \
+	TEXT_BUFFER_VISIBLE_HEIGHT * TEXT_BUFFER_VISIBLE_WIDTH
 #define KB_X 91
 #define KB_Y 35
 
-struct kp_event{
+struct kp_event {
 	struct input_event scan;
 	struct input_event key;
 	struct input_event sync;
 };
 #define KP_EV_SIZE sizeof(struct input_event) * 3
 struct kb_state_t {
-	int layout; // 0-22
-	int previous_layout; // 0-22
+	int layout; // 0-23
+	int base_layout; // 0-23
 	int key_pressed; // 0-15 or -1 for none
 	int cursor;
 };
-static struct kb_state_t state = {
-	.layout = LAYOUT_HIRAGANA_DEFAULT,
-	.previous_layout = LAYOUT_HIRAGANA_DEFAULT,
-	.key_pressed = -1,
-	.cursor = 0
-};
+static struct kb_state_t state = { .layout = LAYOUT_HIRAGANA_DEFAULT,
+				   .base_layout = LAYOUT_HIRAGANA_DEFAULT,
+				   .key_pressed = -1,
+				   .cursor = 0 };
 
-static __u16 text_buffer[TEXT_BUFFER_LENGTH];
+struct dyn_buffer {
+	size_t size;
+	size_t max_size;
+	size_t fvs; // First visible symbol
+	__u16 *data;
+} static text_buffer;
 
 static struct fb_manip *fb;
 static int kp;
@@ -44,143 +49,361 @@ extern inline void draw_symbol_from_char_code(struct fb_manip *fb, int x, int y,
 extern inline void draw_symbol_from_key(struct fb_manip *fb, int x, int y,
 					__u16 fg, __u16 bg, int key, int layout,
 					int selected);
-extern inline void draw_keyboard(struct fb_manip *fb, int x, int y, __u16 fg, __u16 bg, int layout);
-extern inline void draw_key(struct fb_manip *fb, int x, int y, __u16 fg, __u16 bg, int key, int layout, int down);
-extern inline void drawrect(struct fb_manip *fb, int x, int y, int w, int h, __u16 c);
+extern inline void draw_keyboard(struct fb_manip *fb, int x, int y, __u16 fg,
+				 __u16 bg, int layout);
+extern inline void draw_key(struct fb_manip *fb, int x, int y, __u16 fg,
+			    __u16 bg, int key, int layout, int down);
+extern inline void drawrect(struct fb_manip *fb, int x, int y, int w, int h,
+			    __u16 c);
 extern inline void wait_for_sync(struct fb_manip *fb);
+
+#define draw_current_layout()                                                  \
+	draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK, state.layout)
 
 extern inline int read_event(int fd, struct input_event *e, size_t ev_size);
 
-inline int CURSOR_X(void)
+inline int CURSOR_X(int pos)
 {
-	return state.cursor % TEXT_BUFFER_WIDTH * 10;
+	return pos % TEXT_BUFFER_VISIBLE_WIDTH * 10;
 }
-inline int CURSOR_Y(void)
+inline int CURSOR_Y(int pos)
 {
-	return state.cursor / TEXT_BUFFER_WIDTH * 10;
+	return pos / TEXT_BUFFER_VISIBLE_WIDTH * 10;
 }
-extern inline int CURSOR_X(void);
-extern inline int CURSOR_Y(void);
+extern inline int CURSOR_X(int pos);
+extern inline int CURSOR_Y(int pos);
 
+#define CURSOR_IN_BUFFER (state.cursor + text_buffer.fvs)
 
 static void change_with_sound_mark(int shift)
 {
-	if (text_buffer[state.cursor] > 0x3040 + shift) {
-		text_buffer[state.cursor] -= shift;
-		if (text_buffer[state.cursor] < HIRAGANA_LETTER_SMALL_TU) {
-			if (text_buffer[state.cursor] & 1)
-				text_buffer[state.cursor] += 1;
+	if (text_buffer.data[CURSOR_IN_BUFFER] > 0x3040 + shift) {
+		text_buffer.data[CURSOR_IN_BUFFER] -= shift;
+		if (text_buffer.data[CURSOR_IN_BUFFER] <
+		    HIRAGANA_LETTER_SMALL_TU) {
+			if (text_buffer.data[CURSOR_IN_BUFFER] & 1)
+				text_buffer.data[CURSOR_IN_BUFFER] += 1;
 			else
-				text_buffer[state.cursor] -= 1;
-		} else if (text_buffer[state.cursor] < HIRAGANA_LETTER_TE) {
-			if (text_buffer[state.cursor] ==
+				text_buffer.data[CURSOR_IN_BUFFER] -= 1;
+		} else if (text_buffer.data[CURSOR_IN_BUFFER] <
+			   HIRAGANA_LETTER_TE) {
+			if (text_buffer.data[CURSOR_IN_BUFFER] ==
 			    HIRAGANA_LETTER_SMALL_TU)
-				text_buffer[state.cursor] = HIRAGANA_LETTER_DU;
+				text_buffer.data[CURSOR_IN_BUFFER] =
+					HIRAGANA_LETTER_DU;
 			else
-				text_buffer[state.cursor] -= 1;
-		} else if (text_buffer[state.cursor] < HIRAGANA_LETTER_NA) {
-			if (text_buffer[state.cursor] & 1)
-				text_buffer[state.cursor] += 1;
+				text_buffer.data[CURSOR_IN_BUFFER] -= 1;
+		} else if (text_buffer.data[CURSOR_IN_BUFFER] <
+			   HIRAGANA_LETTER_NA) {
+			if (text_buffer.data[CURSOR_IN_BUFFER] & 1)
+				text_buffer.data[CURSOR_IN_BUFFER] -= 1;
 			else
-				text_buffer[state.cursor] -= 1;
-		} else if (text_buffer[state.cursor] < HIRAGANA_LETTER_MA &&
-			   text_buffer[state.cursor] > HIRAGANA_LETTER_NO) {
-			if ((text_buffer[state.cursor] - HIRAGANA_LETTER_HA) %
+				text_buffer.data[CURSOR_IN_BUFFER] += 1;
+		} else if (text_buffer.data[CURSOR_IN_BUFFER] <
+				   HIRAGANA_LETTER_MA &&
+			   text_buffer.data[CURSOR_IN_BUFFER] >
+				   HIRAGANA_LETTER_NO) {
+			if ((text_buffer.data[CURSOR_IN_BUFFER] -
+			     HIRAGANA_LETTER_HA) %
 				    3 ==
 			    2) {
-				text_buffer[state.cursor] -= 2;
+				text_buffer.data[CURSOR_IN_BUFFER] -= 2;
 			} else {
-				text_buffer[state.cursor] += 1;
+				text_buffer.data[CURSOR_IN_BUFFER] += 1;
 			}
-		} else if (text_buffer[state.cursor] < HIRAGANA_LETTER_RA &&
-			   text_buffer[state.cursor] > HIRAGANA_LETTER_MO) {
-			if (text_buffer[state.cursor] & 1)
-				text_buffer[state.cursor] += 1;
+		} else if (text_buffer.data[CURSOR_IN_BUFFER] <
+				   HIRAGANA_LETTER_RA &&
+			   text_buffer.data[CURSOR_IN_BUFFER] >
+				   HIRAGANA_LETTER_MO) {
+			if (text_buffer.data[CURSOR_IN_BUFFER] & 1)
+				text_buffer.data[CURSOR_IN_BUFFER] += 1;
 			else
-				text_buffer[state.cursor] -= 1;
+				text_buffer.data[CURSOR_IN_BUFFER] -= 1;
 		}
-		text_buffer[state.cursor] += shift;
+		text_buffer.data[CURSOR_IN_BUFFER] += shift;
+	}
+}
+
+static int put_symbol_after_cursor(int code)
+{
+	state.cursor++;
+	text_buffer.size++;
+	if (text_buffer.size > text_buffer.max_size) {
+		text_buffer.max_size += TEXT_BUFFER_VISIBLE_LENGTH;
+		text_buffer.data = realloc(text_buffer.data,
+					   text_buffer.max_size *
+						   sizeof(*text_buffer.data));
+	}
+	for (int i = text_buffer.size - 1; i > CURSOR_IN_BUFFER; --i) {
+		text_buffer.data[i] = text_buffer.data[i - 1];
+	}
+	if (text_buffer.size == 1)
+		state.cursor = 0;
+	text_buffer.data[CURSOR_IN_BUFFER] = code;
+
+	if (state.cursor == TEXT_BUFFER_VISIBLE_LENGTH) {
+		text_buffer.fvs++;
+		state.cursor--;
+		return 1;
+	}
+	return 0;
+}
+
+static int delete_symbol_under_cursor(void)
+{
+	if (text_buffer.size <= 0)
+		return 1;
+	text_buffer.size--;
+	for (int i = CURSOR_IN_BUFFER; i < text_buffer.size; ++i) {
+		text_buffer.data[i] = text_buffer.data[i + 1];
+	}
+	text_buffer.data[text_buffer.size] = 0;
+	if (CURSOR_IN_BUFFER == text_buffer.size)
+		state.cursor--;
+	if (state.cursor < 0) {
+		state.cursor = 0;
+		if (text_buffer.fvs != 0) {
+			text_buffer.fvs--;
+			return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+static int move_cursor_left(void)
+{
+	if (CURSOR_IN_BUFFER != 0) {
+		if (state.cursor != 0) {
+			state.cursor--;
+		} else if (text_buffer.fvs != 0) {
+			text_buffer.fvs--;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+static int move_cursor_right(void)
+{
+	if (CURSOR_IN_BUFFER != text_buffer.size - 1) {
+		if (state.cursor != TEXT_BUFFER_VISIBLE_LENGTH - 1) {
+			state.cursor++;
+		} else if (text_buffer.fvs != text_buffer.size - 1) {
+			text_buffer.fvs++;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+static void redraw_full_buffer(void)
+{
+	for (int i = 0; i < TEXT_BUFFER_VISIBLE_LENGTH; ++i) {
+		draw_symbol_from_char_code(
+			fb, CURSOR_X(i), CURSOR_Y(i), GREEN, BLACK,
+			text_buffer.data[text_buffer.fvs + i], IS_NOT_SELECTED);
+	}
+	draw_symbol_from_char_code(
+		fb, CURSOR_X(state.cursor), CURSOR_Y(state.cursor), GREEN,
+		BLACK, text_buffer.data[text_buffer.fvs + state.cursor],
+		IS_SELECTED);
+}
+
+static void redraw_buffer_from_cursor(void)
+{
+	draw_symbol_from_char_code(fb, CURSOR_X(state.cursor),
+				   CURSOR_Y(state.cursor), GREEN, BLACK,
+				   text_buffer.data[CURSOR_IN_BUFFER],
+				   IS_SELECTED);
+	for (int i = state.cursor + 1; i < TEXT_BUFFER_VISIBLE_LENGTH; ++i) {
+		if (text_buffer.data[text_buffer.fvs + i] != 0) {
+			draw_symbol_from_char_code(
+				fb, CURSOR_X(i), CURSOR_Y(i), GREEN, BLACK,
+				text_buffer.data[text_buffer.fvs + i],
+				IS_NOT_SELECTED);
+		} else {
+			draw_symbol_from_char_code(fb, CURSOR_X(i), CURSOR_Y(i),
+						   BLACK, BLACK,
+						   IDEOGRAPHIC_SPACE,
+						   IS_NOT_SELECTED);
+			return;
+		}
 	}
 }
 
 static int on_key_down(int key)
 {
 	printf("%s %d\n", __FUNCTION__, key);
-	state.key_pressed = key;
-	draw_key(fb, KB_X, KB_Y, GREEN, BLACK,
-			key, state.layout, IS_DOWN);
+	if (state.key_pressed == -1)
+		state.key_pressed = key;
+	draw_key(fb, KB_X, KB_Y, GREEN, BLACK, key, state.layout, IS_DOWN);
 	return 0;
 }
 
 static int on_key_up(int key)
 {
 	printf("%s %d\n", __FUNCTION__, key);
+	if (key != state.key_pressed)
+		return 0;
+	switch (key) {
+	case KATAKANA_HIRAGANA_KEY:
+		state.base_layout = LAYOUT_KATAKANA_DEFAULT ^ state.base_layout;
+		state.layout = state.layout % LAYOUT_KATAKANA_DEFAULT +
+			       state.base_layout;
+		goto finilize;
+	case BACKSPACE_KEY:
+		if (state.layout != state.base_layout) {
+			state.layout = state.base_layout;
+			goto finilize;
+		}
+		if (delete_symbol_under_cursor()) {
+			draw_symbol_from_char_code(fb, CURSOR_X(0), CURSOR_Y(0),
+						   BLACK, BLACK,
+						   IDEOGRAPHIC_SPACE,
+						   IS_NOT_SELECTED);
+		} else {
+			redraw_buffer_from_cursor();
+		}
+		goto finilize;
+	case LEFT_KEY:
+		if (state.layout != state.base_layout) {
+			state.layout = state.base_layout;
+			goto finilize;
+		}
+		if (move_cursor_left()) {
+			redraw_buffer_from_cursor();
+		} else {
+			if (text_buffer.size > 1)
+				draw_symbol_from_char_code(
+					fb, CURSOR_X(state.cursor + 1),
+					CURSOR_Y(state.cursor + 1), GREEN,
+					BLACK,
+					text_buffer.data[CURSOR_IN_BUFFER + 1],
+					IS_NOT_SELECTED);
+			draw_symbol_from_char_code(
+				fb, CURSOR_X(state.cursor),
+				CURSOR_Y(state.cursor), GREEN, BLACK,
+				text_buffer.data[CURSOR_IN_BUFFER],
+				IS_SELECTED);
+		}
+		goto finilize;
+	case RIGHT_KEY:
+		if (state.layout != state.base_layout) {
+			state.layout = state.base_layout;
+			goto finilize;
+		}
+		if (move_cursor_right()) {
+			redraw_full_buffer();
+		} else {
+			if (text_buffer.size > 1)
+				draw_symbol_from_char_code(
+					fb, CURSOR_X(state.cursor - 1),
+					CURSOR_Y(state.cursor - 1), GREEN,
+					BLACK,
+					text_buffer.data[CURSOR_IN_BUFFER - 1],
+					IS_NOT_SELECTED);
+			draw_symbol_from_char_code(
+				fb, CURSOR_X(state.cursor),
+				CURSOR_Y(state.cursor), GREEN, BLACK,
+				text_buffer.data[CURSOR_IN_BUFFER],
+				IS_SELECTED);
+		}
+		goto finilize;
+	default:
+		break;
+	}
 	switch (state.layout) {
 	case LAYOUT_HIRAGANA_DEFAULT:
-		state.previous_layout = state.layout;
+		state.base_layout = state.layout;
 		state.layout = layout_switch[state.key_pressed];
-		printf("%s layout %d -> %d\n", __FUNCTION__, state.previous_layout, state.layout);
 		if (state.key_pressed == SOUND_MARKS_KEY) {
 			change_with_sound_mark(0);
-			draw_symbol_from_char_code(fb, CURSOR_X(), CURSOR_Y(),
-						   GREEN, BLACK,
-						   text_buffer[state.cursor],
-						   IS_SELECTED);
+			draw_symbol_from_char_code(
+				fb, CURSOR_X(state.cursor),
+				CURSOR_Y(state.cursor), GREEN, BLACK,
+				text_buffer.data[CURSOR_IN_BUFFER],
+				IS_SELECTED);
 		}
-		draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK, state.layout);
 		break;
 	case LAYOUT_KATAKANA_DEFAULT:
-		state.previous_layout = state.layout;
+		state.base_layout = state.layout;
 		state.layout =
 			layout_to_katakana(layout_switch[state.key_pressed]);
-		printf("%s layout %d -> %d\n", __FUNCTION__, state.previous_layout, state.layout);
 		if (state.key_pressed == SOUND_MARKS_KEY) {
 			change_with_sound_mark(0x30A0 - 0x3040);
-			draw_symbol_from_char_code(fb, CURSOR_X(), CURSOR_Y(),
-						   GREEN, BLACK,
-						   text_buffer[state.cursor],
-						   IS_SELECTED);
+			draw_symbol_from_char_code(
+				fb, CURSOR_X(state.cursor),
+				CURSOR_Y(state.cursor), GREEN, BLACK,
+				text_buffer.data[CURSOR_IN_BUFFER],
+				IS_SELECTED);
 		}
-		draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK, state.layout);
 		break;
 	default:
-		printf("[%d] %d,%d\n", state.cursor, CURSOR_X(), CURSOR_Y());
-		if (text_buffer[state.cursor] != 0)
-			draw_symbol_from_char_code(fb, CURSOR_X(), CURSOR_Y(),
-						   GREEN, BLACK,
-						   text_buffer[state.cursor++],
-						   IS_NOT_SELECTED);
-		printf("[%d] %d,%d\n", state.cursor, CURSOR_X(), CURSOR_Y());
-		draw_symbol_from_key(fb, CURSOR_X(), CURSOR_Y(), GREEN, BLACK,
-				     state.key_pressed, state.layout,
-				     IS_SELECTED);
-		text_buffer[state.cursor] =
-			kb_layouts[state.layout].codes[state.key_pressed];
-		printf("[%d] %d,%d %%/ %d\n", state.cursor, CURSOR_X(), CURSOR_Y(), state.cursor % 8);
-		if (state.layout != state.previous_layout) {
-			state.layout = state.previous_layout;
-			draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK, state.layout);
+		if (kb_layouts[state.layout].codes[state.key_pressed] == 0)
+			break;
+		if (put_symbol_after_cursor(kb_layouts[state.layout].codes[key])) {
+			redraw_full_buffer();
+		} else {
+			if (text_buffer.size > 1) {
+				draw_symbol_from_char_code(
+					fb, CURSOR_X(state.cursor - 1),
+					CURSOR_Y(state.cursor - 1), GREEN,
+					BLACK,
+					text_buffer.data[CURSOR_IN_BUFFER - 1],
+					IS_NOT_SELECTED);
+				redraw_buffer_from_cursor();
+			} else {
+				draw_symbol_from_char_code(
+					fb, CURSOR_X(state.cursor),
+					CURSOR_Y(state.cursor), GREEN, BLACK,
+					text_buffer.data[CURSOR_IN_BUFFER],
+					IS_SELECTED);
+			}
+		}
+		if (state.layout != state.base_layout) {
+			state.layout = state.base_layout;
+			draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK,
+				      state.layout);
 		}
 	}
+
+finilize:
+	draw_current_layout();
 	state.key_pressed = -1;
 	return 0;
 }
 
-typedef int (*ev_handler_t)(int);
-ev_handler_t handlers[2] = {
-	on_key_up,
-	on_key_down
-};
-static void sigint_handler(int signum)
+
+static void init_buffer(void)
 {
-	release_fb(fb);
-	release_keypad(kp);
+	text_buffer.max_size = TEXT_BUFFER_VISIBLE_LENGTH;
+	text_buffer.data =
+		malloc(TEXT_BUFFER_VISIBLE_LENGTH * sizeof(*text_buffer.data));
 }
+
+static void print_hello(void)
+{
+	put_symbol_after_cursor(HIRAGANA_LETTER_MI);
+	put_symbol_after_cursor(HIRAGANA_LETTER_N);
+	put_symbol_after_cursor(HIRAGANA_LETTER_NA);
+	put_symbol_after_cursor(HIRAGANA_LETTER_SA);
+	put_symbol_after_cursor(HIRAGANA_LETTER_N);
+	put_symbol_after_cursor(IDEOGRAPHIC_COMMA);
+	put_symbol_after_cursor(HIRAGANA_LETTER_KO);
+	put_symbol_after_cursor(HIRAGANA_LETTER_N);
+	put_symbol_after_cursor(HIRAGANA_LETTER_NI);
+	put_symbol_after_cursor(HIRAGANA_LETTER_TI);
+	put_symbol_after_cursor(HIRAGANA_LETTER_HA);
+	put_symbol_after_cursor(IDEOGRAPHIC_FULL_STOP);
+}
+
+typedef int (*ev_handler_t)(int);
+ev_handler_t handlers[2] = { on_key_up, on_key_down };
 
 int main(int argc, char *argv[])
 {
 	struct kp_event e;
-	signal(SIGINT, sigint_handler);
 	fb = grab_fb("/dev/fb1");
 	if (fb == NULL)
 		return -1;
@@ -189,8 +412,12 @@ int main(int argc, char *argv[])
 		release_fb(fb);
 		return -1;
 	}
+	init_buffer();
 	clear_screen(fb, BLACK);
 	draw_keyboard(fb, KB_X, KB_Y, GREEN, BLACK, state.layout);
+	print_hello();
+	state.cursor = 0;
+	redraw_buffer_from_cursor();
 	while (1) {
 		if (read_event(kp, (struct input_event *)&e, KP_EV_SIZE))
 			break;
@@ -199,17 +426,6 @@ int main(int argc, char *argv[])
 	}
 	release_fb(fb);
 	release_keypad(kp);
+	free(text_buffer.data);
 	return 0;
-		//draw_symbol_from_char_code(fb, 1, 1, GREEN, BLACK, HIRAGANA_LETTER_KO,
-		//			   IS_NOT_SELECTED);
-		//draw_symbol_from_char_code(fb, 13, 1, GREEN, BLACK, HIRAGANA_LETTER_N,
-		//			   IS_NOT_SELECTED);
-		//draw_symbol_from_char_code(fb, 25, 1, GREEN, BLACK, HIRAGANA_LETTER_NI,
-		//			   IS_NOT_SELECTED);
-		//draw_symbol_from_char_code(fb, 37, 1, GREEN, BLACK, HIRAGANA_LETTER_TI,
-		//			   IS_NOT_SELECTED);
-		//draw_symbol_from_char_code(fb, 49, 1, GREEN, BLACK, HIRAGANA_LETTER_HA,
-		//			   IS_NOT_SELECTED);
-		//draw_symbol_from_char_code(fb, 61, 1, GREEN, BLACK,
-		//			   IDEOGRAPHIC_FULL_STOP, IS_NOT_SELECTED);
 }
